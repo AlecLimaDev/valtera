ce085172c965c897c1432ddf658317e9d31e24ad
Alec Lima <aleclimadev@gmail.com>
2025-07-03 21:43:27 -0300

    feat: data display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { goto } from "$app/navigation";
+  import { browser } from "$app/environment";
+  import Input from "$lib/input.svelte";
+  import UserList from "$lib/user-list.svelte";
+  export let data;
+
+  let searchName = "";
+  let searchNameDebounced = "";
+  let timeout;
+
+  $: {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      searchNameDebounced = searchName;
+    }, 1000);
+  }
+
+  $: if (browser) {
+    if (searchNameDebounced.trim()) {
+      goto(`?name=${encodeURIComponent(searchNameDebounced.trim())}`);
+    } else if (searchNameDebounced.length === 0) {
+      goto("?");
+    }
+  }
+</script>
+
+<h1>Estimativa de nome - Valtera</h1>
+<Input bind:value={searchName} placeholder="Digite seu nome" />
+{#if data.fetchData}
+  <UserList name={data.fetchData.name} age={data.fetchData.age} />
+{:else if data.searchName}
+  <span>Nenhum resultado encontrado para "{data.searchName}"</span>
+{:else}
+  <span>Digite um nome para começar a pesquisa.</span>
+{/if}
