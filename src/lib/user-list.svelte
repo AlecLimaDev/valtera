79b6b0782e2d2516eeca7b0625fa71ac03430b75
Alec Lima <aleclimadev@gmail.com>
2025-07-03 21:44:44 -0300

    chore: user-list component

---
+++ src/lib/user-list.svelte
@@ -0,0 +1,18 @@
+<script>
+  let { name, age } = $props();
+</script>
+
+<div class="response-data">
+  <span>Nome: {name}</span>
+  <span>Idade: {age}</span>
+</div>
+
+<style>
+  .response-data {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-top: 60px;
+    font-size: 24px;
+  }
+</style>
