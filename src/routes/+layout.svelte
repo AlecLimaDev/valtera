66237ac725248dd2542bfc1ccdebec1e981e8521
Alec Lima <aleclimadev@gmail.com>
2025-07-03 21:43:49 -0300

    feat: layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<main class="container">
+  <slot />
+</main>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 80vh;
+    font-family: sans-serif;
+  }
+</style>
